231a536934cdfd4739e89c9bcca8128820376045
Jordi Ayala <jord7as@gmail.com>
2023-03-03 17:17:56 -0600

    refactor: ♻️ reorder commit types

--- src/commit-types.js
+++ src/commit-types.js
@@ -1,24 +1,4 @@
 export const COMMIT_TYPES = {
-  build: {
-    emoji: '🏗️',
-    description: 'Changes that affect the build system or external dependencies',
-    release: false
-  },
-  chore: {
-    emoji: '🤖',
-    description: 'Other changes that don\'t modify src or test files',
-    release: false
-  },
-  ci: {
-    emoji: '👷',
-    description: 'Changes to our CI configuration files and scripts',
-    release: false
-  },
-  docs: {
-    emoji: '📝',
-    description: 'Documentation only changes',
-    release: false
-  },
   feat: {
     emoji: '✨',
     description: 'A new feature',
@@ -29,29 +9,49 @@ export const COMMIT_TYPES = {
     description: 'A bug fix',
     release: true
   },
-  perf: {
-    emoji: '⚡️',
-    description: 'A code change that improves performance',
-    release: true
-  },
-  refactor: {
-    emoji: '♻️',
-    description: 'A code change that neither fixes a bug nor adds a feature',
-    release: true
-  },
-  revert: {
-    emoji: '⏪️',
-    description: 'Reverts a previous commit',
-    release: true
+  docs: {
+    emoji: '📝',
+    description: 'Documentation only changes',
+    release: false
   },
   style: {
     emoji: '💄',
     description: 'Changes that do not affect the meaning of the code',
     release: false
   },
+  refactor: {
+    emoji: '♻️',
+    description: 'A code change that neither fixes a bug nor adds a feature',
+    release: true
+  },
+  perf: {
+    emoji: '⚡️',
+    description: 'A code change that improves performance',
+    release: true
+  },
   test: {
     emoji: '🧪',
     description: 'Add or update tests',
     release: false
-  }
+  },
+  build: {
+    emoji: '🏗️',
+    description: 'Changes that affect the build system or external dependencies',
+    release: false
+  },
+  ci: {
+    emoji: '👷',
+    description: 'Changes to our CI configuration files and scripts',
+    release: false
+  },
+  chore: {
+    emoji: '🤖',
+    description: 'Other changes that don\'t modify src or test files',
+    release: false
+  },
+  revert: {
+    emoji: '⏪️',
+    description: 'Reverts a previous commit',
+    release: true
+  },
 };
